bd2a7b88e00938281000d5bfac1ffb9b9134f886
Sma1K <37871907+Sma1K@users.noreply.github.com>
2022-05-17 19:21:58 +0300

    upd js

--- task2.js
+++ task2.js
@@ -1,20 +1,24 @@
 let glob = [];
-let arr = {"key1" : [5, 7, 3, 5], "key2" : ["gdf1", "fgdf2", "gdf3", "fgdf4"], "key3" : [[1, 2], [3, 4], [5, 6], [7, 8]]};
-    
-    //console.log((arr["key1"]).length);
-    
+let arr = {
+    "key1": [5, 7, 3, 5],
+    "key2": ["gdf1", "fgdf2", "gdf3", "fgdf4"],
+    "key3": [
+        [1, 2],
+        [3, 4],
+        [5, 6],
+        [7, 8]
+    ]
+};
 
-    for (let i = 0; i < (arr["key1"]).length; i++) {
-      
-        let test = [];
-        Object.keys(arr).forEach((key, val) => {
-          //console.log(key+i);
-            let tmp = {[key]:arr[key][i]};
-            test.push(tmp);
-       
-        })
-        
-        glob.push(test);
-      }
-      console.log(glob);
-        
+for (let i = 0; i < (arr["key1"]).length; i++) {
+    let test = {};
+    Object.keys(arr).forEach((key, val) => {
+
+        let tmp = {
+            [key]: arr[key][i]
+        };
+        test = Object.assign(test, tmp);
+    })
+    glob.push(test);
+}
+console.log(glob);
